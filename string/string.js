879372b998359aaf849e6a4293f31da8c3b2da5f
Stephen J. Carnam <steve@steveorevo.com>
2017-10-04 23:41:43 -0700

    Remove var_dump debug function

--- string/string.js
+++ string/string.js
@@ -94,38 +94,5 @@ module.exports = function(RED) {
             this.send(msg);
         });
     }
-
-
-    function var_dump(arr, level) {
-        var dumped_text = "";
-        if (!level)
-            level = 5;
-
-        //The padding given at the beginning of the line.
-        var level_padding = "";
-        for (var j = 0; j < level + 1; j++)
-            level_padding += "    ";
-
-        if (typeof(arr) === 'object') { //Array/Hashes/Objects
-            for (var item in arr) {
-                var value = arr[item];
-
-                if (typeof(value) === 'object') { //If it is an array,
-                    dumped_text += level_padding + "'" + item + "' ...\n";
-                    dumped_text += var_dump(value, level + 1);
-                } else {
-                    dumped_text += level_padding + "'" + item + "' => " + "(" + typeof(value) + ") \"" + value + "\"\n";
-                }
-            }
-        } else { //Stings/Chars/Numbers etc.
-            dumped_text = "(" + typeof(arr) + ") " + arr;
-            return dumped_text;
-        }
-        if (level===0){
-            return '(object)' + String.fromCharCode(10) + dumped_text;
-        }else{
-            return dumped_text;
-        }
-    }
     RED.nodes.registerType("string", string);
 }
